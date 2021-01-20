4d857bb5570830b87dc4cc8656b0523e414a8b74
Vincent Martinet <martinet.v@sfeir.com>
2021-01-20 22:55:15 +0100

    init

---
+++ src/test/java/com/sfeir/kata/bankaccount/StepDefinitions.java
@@ -0,0 +1,11 @@
+package com.sfeir.kata.bankaccount;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+}
